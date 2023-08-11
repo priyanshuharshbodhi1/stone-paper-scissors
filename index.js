f05afbda22a8a6137e022a324a578e82de50d2b6
priyanshu <priyanshuqpwp@gmail.com>
2023-08-11 17:57:12 +0530

    mobile UI completed

---
+++ index.js
@@ -0,0 +1,18 @@
+const openRulesButton = document.getElementById('openRulesButton');
+const gameRulesPopup = document.getElementById('gameRulesPopup');
+const closeButton = document.getElementById('closeButton');
+
+openRulesButton.addEventListener('click', () => {
+  gameRulesPopup.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  gameRulesPopup.style.display = 'none';
+});
+
+  
+  
+  
+  
+  
+  
